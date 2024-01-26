73bc94e29ede2a68d5e8c8c41444361e251f8c33
Ramla Rizvi <iamramlarizvi@gmail.com>
2024-01-26 20:17:48 +0530

    signup page

---
+++ signup.js
@@ -0,0 +1,21 @@
+function cancelSignUp() {
+
+    alert("Sign up canceled");
+  }
+  
+  function registerUser() {
+   
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("psw").value;
+    var confirmPassword = document.getElementById("psw-repeat").value;
+  
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+  
+   
+    alert("User registered!\nEmail: " + email);
+  };
+        
